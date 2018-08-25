5572c9b0e96d71084d7fc5a965a3c7e734953617
codecrook <hello@codecrook.io>
2018-08-26 03:21:06 +0530

    Modified the 'create new' functionality
    What's Done:
    
    - Added functionality to show popup to choose wheteher the user wants to create new folder or new file, when the create new button is clicked.
    - Added functionality to create new file

--- js/app.js
+++ js/app.js
@@ -2,9 +2,14 @@
     'use strict';
 
     let folders;
-    const newButton = document.querySelector('.new-button');
+    let files;
+    const createNewButton = document.querySelector('.new-button');
+    const createNewFileOption = document.querySelector('.create-new-file');
+    const createNewFolderOption = document.querySelector('.create-new-folder');
 
-    newButton.addEventListener('click', createFolder);
+    createNewButton.addEventListener('click', showCreateNewOption);
+    createNewFolderOption.addEventListener('click', createNewFolder);
+    createNewFileOption.addEventListener('click', createNewFile);
 
     function selectItem(e) {
         e.target.classList.toggle('selected-item');
@@ -15,7 +20,7 @@
         e.target.classList.add('folder-open');
     }
 
-    function createFolder(e) {
+    function createNewFolder(e) {
         let mainContainer = document.querySelector('.main-container'); 
         let innerContent = mainContainer.innerHTML;
         let folderName = prompt('Folder Name:', 'untitled folder');
@@ -32,11 +37,36 @@
         initialize();
     }
 
+    function createNewFile(e) {
+        let mainContainer = document.querySelector('.main-container'); 
+        let innerContent = mainContainer.innerHTML;
+        let fileName = prompt('File Name:', 'new file');
+
+        if (!fileName) return; //won't create folder if cancel is clicked on the prompt
+        
+        let newFileCode = `<div class="file-all file">
+                                <span class="folder-name">${fileName}</span>
+                            </div>`;
+        
+        innerContent += newFileCode;
+        mainContainer.innerHTML = innerContent;
+
+        initialize();
+    }
+
     function initialize() {
-        folders = document.querySelectorAll('.folder')
+        folders = document.querySelectorAll('.folder');
+        files = document.querySelectorAll('.file-all');
+
         folders.forEach(folder => folder.addEventListener('dblclick', openFolder));
         folders.forEach(folder => folder.addEventListener('click', selectItem));
+        files.forEach(file => file.addEventListener('click', selectItem));
     };
+
+    function showCreateNewOption(e) {
+        var createItemPopup = document.querySelector(".create-new-popup");
+        createItemPopup.classList.toggle("show");
+    }
 
     initialize();
 
