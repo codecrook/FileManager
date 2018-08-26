fae6dcc4fd7265e5b26587ff703471e9270aa0df
codecrook <hello@codecrook.io>
2018-08-26 06:51:28 +0530

    Added file creation functionality
    What's Done:
    
    - Added function to create file and download it to local system when new file button is clicked

--- js/app.js
+++ js/app.js
@@ -96,6 +96,8 @@
         innerContent += newFileCode;
         mainContainer.innerHTML = innerContent;
 
+        download('\0', fileName, fileExtension);
+
         initialize();
     }
 
@@ -138,6 +140,30 @@
         
         selectedCountDisplay.innerHTML = `${selectedItems.length} item(s) deleted!`;
         setTimeout(() => selectedCountDisplay.style.visibility = "hidden", 1000);
+    }
+
+    /*function to create a file and download it to local system
+     *code copied from stackoverflow
+     */
+    function download(data, filename, type) {
+        var file = new Blob([data], {type: type}); // creates file using BLOB
+        if (window.navigator.msSaveOrOpenBlob) {
+            // IE10+
+            window.navigator.msSaveOrOpenBlob(file, filename);
+        } else { // Others
+            var a = document.createElement("a"),
+            url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            
+            //removes the link created for download immidiately
+            setTimeout(function() {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);  
+            }, 0); 
+        }
     }
 
     initialize();
